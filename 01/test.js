94232f98ab1ae51d4c66cb388185922f45aaa67d
HighonKode-murli <128061669+HighonKode-murli@users.noreply.github.com>
2024-12-26 16:26:51 +0000

    comparison operators

--- 01/test.js
+++ 01/test.js
@@ -1,1 +1,10 @@
-console.log("hi")
+//alert()  //this don't work in node env. Works on only browser
+
+// 1=> true , 0=> false
+// "" =>false, "xxx" => true
+
+//console.log(null>0) => false
+//console.log(null==0) => false         // js works differently on comparisons and equal opertaors
+//console.log(null>=0) => true         // for > and >= it convets null to 0 , whereas for equal it does not.
+
+
